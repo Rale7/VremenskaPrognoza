352f9f31a60359832da62670c54afd8bd28367b2
Rale <radovanjevri@gmail.com>
2025-02-26 00:00:52 +0100

    Popravljena greska pri menjanju velicine prozora

--- ViewModel/MenuViewModel.cs
+++ ViewModel/MenuViewModel.cs
@@ -47,15 +47,39 @@ namespace VremenskaPrognoza.ViewModel
                 wnd.WindowState = WindowState.Minimized;
             }
         }
+    
+        private double prevLeft, prevTop, prevHeight, prevWidth;
+        private bool isMaximized = false;
 
         private void MaximizeWindow(object window)
         {
             if (window is Window wnd)
             {
-                wnd.WindowState = wnd.WindowState == WindowState.Maximized
-                ? WindowState.Normal
-                : WindowState.Maximized;
-            }
+                var screen = SystemParameters.WorkArea;
+
+                if (isMaximized)
+                {
+                    wnd.Left = prevLeft;
+                    wnd.Top = prevTop;
+                    wnd.Width = prevWidth;
+                    wnd.Height = prevHeight;
+                    isMaximized = false;
+                }
+                else
+                {
+                    prevLeft = wnd.Left;
+                    prevTop = wnd.Top;
+                    prevWidth = wnd.Width;
+                    prevHeight = wnd.Height;
+
+                    wnd.Left = screen.Left;
+                    wnd.Top = screen.Top;
+                    wnd.Width = screen.Width;
+                    wnd.Height = screen.Height;
+
+                    isMaximized = true;
+                }
+            }    
         }
     }
 }
